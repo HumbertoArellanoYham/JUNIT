c8e8efb79962a31a2971053b2dfa7fbc9ce5819b
HumbertoArellano <humbertoyham25@gmail.com>
2024-01-25 11:32:24 -0700

    Clase cuenta

---
+++ src/main/java/org/arellano/junitapp/ejemplo/Cuenta.java
@@ -0,0 +1,29 @@
+package org.arellano.junitapp.ejemplo;
+
+import java.math.BigDecimal;
+
+public class Cuenta {
+    private String nombre;
+    private BigDecimal saldo;
+
+    public Cuenta(String nombre, BigDecimal saldo){
+        this.nombre = nombre;
+        this.saldo = saldo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+}
